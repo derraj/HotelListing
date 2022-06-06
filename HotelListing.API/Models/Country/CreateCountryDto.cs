e1a13ef23415835bffa45006cdb98f4168601708
jarredcm <mahinay@ualberta.ca>
2022-06-06 06:33:05 -0600

    Refactor PUT Methods to use Dto.

--- HotelListing.API/Models/Country/CreateCountryDto.cs
+++ HotelListing.API/Models/Country/CreateCountryDto.cs
@@ -4,10 +4,8 @@ namespace HotelListing.API.Models.Country
 {
     //prevent overposting and overexposing data
     // must have same name as Model
-    public class CreateCountryDto
+    public class CreateCountryDto : BaseCountryDto
     {
-        [Required]
-        public string Name { get; set; }
-        public string ShortName { get; set; }
+
     }
 }
